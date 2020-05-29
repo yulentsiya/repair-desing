22d028f672713a8cc84df080bb2535dc21ca0184
yulentsiya <yulentsiyaa@mail.ru>
2020-05-29 19:14:38 +0300

    feat: add Futura book & Demi fonts

--- my-project/gulpfile.js
+++ my-project/gulpfile.js
@@ -1,6 +1,7 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 
 // Static server 
@@ -13,12 +14,16 @@ function bs() {
   });
   watch("app/*.html").on('change', browserSync.reload);
   watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
   };
 
 function serveSass() {
-  return src("./sass/*.sass")
+  return src("./sass/**/*.sass","./sass/**/*.scss")
     .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false
+  }))
     .pipe(dest("./css"))
     .pipe(browserSync.stream());
 };

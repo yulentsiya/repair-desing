1846232e4bc99fb62d78be7041713bc2e6c57f1e
yulentsiya <yulentsiyaa@mail.ru>
2020-06-08 12:13:47 +0300

    переписали файл main.js с языка js на библиотеку jQuery

--- my-project/js/main.js
+++ my-project/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+/*document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]')
   const closeBtn = document.querySelector('.modal__close')
@@ -13,6 +13,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
   closeBtn.addEventListener('click', switchModal)
 
 });
+*/
 
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
 
-//ll
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+});
